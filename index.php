f8356581f429dacc1ec96028d6f2c355dd951338
Siim Marten Schmidt <siimmarten.schmidt@proton.me>
2025-12-30 10:49:36 +0200

    Add array examples and variables

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Hello world!\n";
+echo 'Kasuta ühekordesid sulgusid';
+$var = 1;
+echo $var;
+
+// Tekstide liitmine punktiga
+echo $var . "\n";
+
+$array1 = array(1, 2, 3, 4);
+$array2 = [true, NULL, 'hello', 1, [1,2,3]];
+
+$array3 = [
+    1,
+    2,
+    3,
+    'name' => 'fkdjdf'
+    'age' => 99
+    'boolean' => true
+]
+
+$array4 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+array_push($array, 5,6,7);
+$array[] = 8;
+var_dump($array2);
