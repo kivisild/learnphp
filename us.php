9bccdb1668368a7f4421e48af7330e76fe64d48e
Siim Marten Schmidt <siimmarten.schmidt@proton.me>
2025-12-31 13:26:05 +0200

    Site example

--- us.php
+++ us.php
@@ -1,37 +1,33 @@
 <?php
 $posts = [
   [
-    'title' => 'World news 1',
+    'title' => 'US news 1',
     'published' => '16.09.2025',
     'author' => 'Siim',
-    'body' => 'Some world news 1'
+    'body' => 'Some US news 1'
   ],
   [
-    'title' => 'World news 2',
+    'title' => 'US news 2',
     'published' => '16.12.2025',
     'author' => 'Igor',
-    'body' => 'Some world news 2'
+    'body' => 'Some US news 2'
   ],
   [
-    'title' => 'World news 3',
+    'title' => 'US news 3',
     'published' => '04.11.2025',
     'author' => 'Svetlana',
-    'body' => 'Some world news 3'
+    'body' => 'Some US news 3'
   ],
   [
-    'title' => 'World news 4',
+    'title' => 'US news 4',
     'published' => '21.01.2025',
     'author' => 'Danil',
-    'body' => 'Some world news 4'
+    'body' => 'Some US news 4'
   ]
   ]
   ?>
-
-
   <?php include __DIR__ . '/partials/header.php'; ?>
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
       <div class="row g-5">
         <div class="col-md-8">
           <?php include __DIR__ . '/partials/posts.php'; ?>
