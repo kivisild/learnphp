4ee6092c85c237b35698d27688fcfcab5db34c72
Siim Marten Schmidt <siimmarten.schmidt@proton.me>
2025-12-31 13:48:50 +0200

    Add simple router script

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// var_dump($_SERVER);
+
+switch($_SERVER['REQUEST_URI']){
+    case '/':
+        include __DIR__ . '/../views/index.php';
+        break;
+    case '/us':
+        include __DIR__ . '/../views/us.php';
+        break;
+    default:
+        echo '404 page not found';
+    
+}
---
+++ server.sh
@@ -0,0 +1,1 @@
+php -S localhost:1337 public/index.php
